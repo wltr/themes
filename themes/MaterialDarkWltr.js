3be99326180c17e22154b95116b6b4257c96c39d
Johannes Walter <jwalter@headlandstech.com>
2022-01-18 12:45:52 -0500

    Add MaterialDarkWltr color theme

---
+++ themes/MaterialDarkWltr.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#546E7A", "#FF5252", "#5CF19E", "#FFD740", "#40C4FF", "#FF4081", "#64FCDA", "#FFFFFF", "#B0BEC5", "#FF8A80", "#B9F6CA", "#FFE57F", "#80D8FF", "#FF80AB", "#A7FDEB", "#FFFFFF"]);
+t.prefs_.set('foreground-color', "#ECEFF1");
+t.prefs_.set('background-color', "#263238");
+t.prefs_.set('cursor-color', '#ECEFF1');
