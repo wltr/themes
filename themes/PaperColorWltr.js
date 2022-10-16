da5bc5a7af8d19221e0cd60537b588132cf16679
Johannes Walter <johannes@wltr.io>
2022-10-16 16:48:09 -0500

    Add PaperColorWltr color theme

---
+++ themes/PaperColorWltr.js
@@ -0,0 +1,27 @@
+black        = '#4d4c4b';
+red          = '#d6225e';
+green        = '#718b00';
+yellow       = '#d65e00';
+blue         = '#4270ad';
+magenta      = '#8958a7';
+cyan         = '#3e989e';
+white        = '#eeeded';
+
+lightBlack   = '#4d4c4b';
+lightRed     = '#d6225e';
+lightGreen   = '#718b00';
+lightYellow  = '#d65e00';
+lightBlue    = '#4270ad';
+lightMagenta = '#8958a7';
+lightCyan    = '#3e989e';
+lightWhite   = '#eeeded';
+
+t.prefs_.set('color-palette-overrides', [
+    black, red, green, yellow, blue, magenta, cyan, white,
+    lightBlack, lightRed, lightGreen, lightYellow,
+    lightBlue, lightMagenta, lightCyan, lightWhite
+]);
+
+t.prefs_.set('foreground-color', "#4d4c4b");
+t.prefs_.set('background-color', "#eeeded");
+t.prefs_.set('cursor-color', '#000000');
