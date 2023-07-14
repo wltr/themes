412d676ae3c162dd483c7a8a7082e9a45abe4a90
Johannes Walter <jwalter@headlandstech.com>
2023-07-14 18:22:10 -0400

    Add OneDarkCool color theme

---
+++ themes/OneDarkCool.js
@@ -0,0 +1,28 @@
+black       = '#151820';
+red         = '#ef5f6b'; // red
+green       = '#97ca72'; // green
+yellow      = '#ebc275'; // yellow
+blue        = '#5ab0f6'; // blue
+magenta     = '#ca72e4'; // pink
+cyan        = '#4dbdcb'; // cyan
+white       = '#ffffff'; // light gray
+lightBlack  = '#7d899f'; // medium gray
+lightRed    = '#ef5f6b'; // red
+lightGreen  = '#97ca72'; // green
+lightYellow = '#ebc275'; // yellow
+lightBlue   = '#5ab0f6'; // blue
+lightMagenta= '#ca72e4'; // pink
+lightCyan   = '#4dbdcb'; // cyan
+lightWhite  = '#ffffff'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [  black     , red         , green     , yellow     ,
+                    blue      , magenta     , cyan      , white      ,
+                    lightBlack, lightRed    , lightGreen, lightYellow,
+                    lightBlue , lightMagenta, lightCyan , lightWhite
+                 ]
+            );
+
+t.prefs_.set('foreground-color', "#a5b0c5");
+t.prefs_.set('background-color', "#242b38");
+t.prefs_.set('cursor-color', '#ffffff');
